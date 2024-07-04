efe8397e99ad57bd8f4dccb1886deb5f99c8ba06
yeolsss <yeol8810@gmail.com>
2024-07-04 10:58:58 +0900

    #3 UI(button Design): 재사용가능한 button component 완성

--- src/app/(rootLayout)/(home)/page.tsx
+++ src/app/(rootLayout)/(home)/page.tsx
@@ -1,3 +1,5 @@
+import Button from '@/components/atoms/button';
+
 const Home = () => {
   return (
     <>
@@ -5,6 +7,32 @@ const Home = () => {
       <div className="text-[50px] font-bold">르탄이의 아카이브</div>
       <div className="text-5xl font-bold">르탄이의 아카이브</div>
       <div className="text-[40px] font-bold">르탄이의 아카이브</div>
+      <div>
+        <Button>primary</Button>
+        <Button disabled={true}>primary</Button>
+        <Button size="sm">primary</Button>
+        <Button size="sm" className="w-[300px] bg-amber-500">
+          primary
+        </Button>
+      </div>
+      <div>
+        <Button variant="secondary">secondary</Button>
+        <Button variant="secondary" disabled={true}>
+          secondary
+        </Button>
+        <Button variant="secondary" size="sm">
+          secondary
+        </Button>
+      </div>
+      <div>
+        <Button variant="submit">submit</Button>
+        <Button variant="submit" disabled={true}>
+          submit
+        </Button>
+        <Button variant="submit" size="sm">
+          submit
+        </Button>
+      </div>
     </>
   );
 };
---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,56 @@
+import { ComponentProps, ReactNode } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { classMerge } from '@/lib/utils';
+
+const buttonVariants = cva(
+  'rounded-full border cursor-pointer data-[disabled=true]:cursor-not-allowed',
+  {
+    variants: {
+      variant: {
+        primary:
+          'bg-primary-400 text-white border-primary-400 hover:bg-primary-600 hover:border-primary-600 data-[disabled=true]:bg-primary-100 data-[disabled=true]:border-primary-100',
+        secondary:
+          'bg-white text-black border-blue-100 hover:bg-blue-55 hover:border-blue-100 data-[disabled=true]:bg-white data-[disabled=true]:border-blue-55 data-[disabled=true]:text-gray-400',
+        submit:
+          'bg-black text-white border-black hover:text-gray-400 data-[disabled=true]:bg-gray-200 data-[disabled=true]:border-gray-200 data-[disabled=true]:text-white',
+      },
+      size: {
+        l: 'py-5 w-40',
+        sm: 'py-3 px-5',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'l',
+    },
+  },
+);
+
+interface ButtonProps
+  extends ComponentProps<'button'>,
+    VariantProps<typeof buttonVariants> {
+  children: ReactNode;
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button = ({
+  children,
+  disabled = false,
+  className,
+  variant,
+  size,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      data-disabled={disabled}
+      className={classMerge(buttonVariants({ variant, size }), className)}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
