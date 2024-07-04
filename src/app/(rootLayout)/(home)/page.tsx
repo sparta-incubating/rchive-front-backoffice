343f2050c53d795c6f9d10ca3789abfe847f9fd6
yeolsss <yeol8810@gmail.com>
2024-07-04 09:57:14 +0900

    #2 init design setting: 디자인 초기세팅

--- src/app/(rootLayout)/(home)/page.tsx
+++ src/app/(rootLayout)/(home)/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="text-6xl font-bold">르탄이의 아카이브</div>
+      <div className="text-[50px] font-bold">르탄이의 아카이브</div>
+      <div className="text-5xl font-bold">르탄이의 아카이브</div>
+      <div className="text-[40px] font-bold">르탄이의 아카이브</div>
+    </>
+  );
 };
 
 export default Home;
