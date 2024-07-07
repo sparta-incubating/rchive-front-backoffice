782f7b582b10837228c1de54d1361b8bb30d3d2c
yeji Sung <dpwl0332@naver.com>
2024-07-08 04:07:22 +0900

    [#4] chore : 폴더 이동

--- src/app/(rootLayout)/(home)/page.tsx
+++ src/app/(rootLayout)/(home)/page.tsx
@@ -11,45 +11,12 @@ import PhoneForm from '@/components/molecules/form/PhoneForm';
 const Home = () => {
   return (
     <>
-      {/* <div className="text-6xl font-bold">르탄이의 아카이브</div>
-      <div className="text-[50px] font-bold">르탄이의 아카이브</div>
-      <div className="text-5xl font-bold">르탄이의 아카이브</div>
-      <div className="text-[40px] font-bold">르탄이의 아카이브</div>
-      <div>
-        <Button>primary</Button>
-        <Button disabled={true}>primary</Button>
-        <Button size="sm">primary</Button>
-        <Button size="sm" className="w-[300px] bg-amber-500">
-          primary
-        </Button>
-      </div>
-      <div>
-        <Button variant="secondary">secondary</Button>
-        <Button variant="secondary" disabled={true}>
-          secondary
-        </Button>
-        <Button variant="secondary" size="sm">
-          secondary
-        </Button>
-      </div>
-      <div>
-        <Button variant="submit">submit</Button>
-        <Button variant="submit" disabled={true}>
-          submit
-        </Button>
-        <Button variant="submit" size="sm">
-          submit
-        </Button>
-      </div> */}
       {/* 회원가입 UI */}
-      <EmailForm /> <br />
-      <PasswordForm /> <br />
-      <NameForm /> <br />
+      <EmailForm />
+      <PasswordForm />
+      <NameForm />
       <PhoneForm />
-      <br />
-      <BirthdayForm /> <br />
-      <br />
-      <br />
+      <BirthdayForm />
     </>
   );
 };
