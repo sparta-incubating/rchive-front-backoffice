9935f15d08da1770519b58bc0dbd1e5226a92f2b
yeji Sung <dpwl0332@naver.com>
2024-07-10 20:22:08 +0900

    [#12] script/chore : axios, query set up & zod 유효성검사 테스트

--- src/app/(rootLayout)/(home)/page.tsx
+++ src/app/(rootLayout)/(home)/page.tsx
@@ -1,9 +1,84 @@
-import SignIn from '@/components/pages/SignIn';
+'use client';
+
+import Button from '@/components/atoms/button';
+import Input from '@/components/atoms/input';
+import { loginSchema } from '@/validators/auth/login.validator';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof loginSchema>) => {
+    try {
+      console.log('제ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ발', data);
+    } catch (error) {
+      console.error('런타임에러', error);
+    }
+
+    // type USER = {
+    //   username: string;
+    //   password: string;
+    // };
+  };
+  // const loginUser: USER = {
+  //   username: 'email,',
+  //   password: 'password',
+  // };
+
+  // client
+  // .post("/api/v1/users/login",loginUser)
+  // .then((res)=>{
+  //   const {accessToken} = res.data;
+  //   cookies.set("Login",accessToken,{
+  //     path : "/",
+  //     secure : true,
+  //     sameStie:"none",
+  //   })
+  // })
   return (
     <>
-      <SignIn />
+      {/* <div className="h-[1024px] w-[1440px] border">1440</div>
+      <div className="h-[1024px] w-[1920px] border">1920</div> */}
+
+      <br />
+      <br />
+      <div>
+        <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+          <div className="border">
+            <Input type="text" placeholder="이메일" {...register('email')} />
+          </div>
+          <br />
+          <span>{errors.email?.message}</span>
+          <br />
+          <div className="border">
+            <Input
+              type="password"
+              placeholder="비밀번호 "
+              {...register('password')}
+            />
+            <br />
+            <span>{errors.password?.message}</span>
+          </div>
+
+          <br />
+          <Button type="submit" disabled={false} className="mb-5 w-80 px-7">
+            다음
+          </Button>
+        </form>
+      </div>
     </>
   );
 };
---
+++ src/axois/axiosClient.ts
@@ -0,0 +1,38 @@
+'use client';
+
+import axios from 'axios';
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export const client = axios.create({
+  baseURL: BACKEND_URL,
+});
+
+// ex) 사용예시
+// const {data} = await api.get(/api/v1/users/login);
+
+client.interceptors.request.use(
+  (config) => {
+    console.log('인터셉트 성공');
+
+    return config;
+  },
+
+  (error) => {
+    console.log('요청 에러');
+    return Promise.reject(error);
+  },
+);
+
+client.interceptors.response.use(
+  (response) => {
+    console.log('응답 받음!');
+
+    return response;
+  },
+
+  (error) => {
+    console.log('응답 에러');
+    return Promise.reject(error);
+  },
+);
