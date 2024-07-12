86fc2f5395fe4b751423be5708a7e3e4895d0f7c
yeolsss <yeol8810@gmail.com>
2024-07-12 09:26:56 +0900

    feat(signup): - modal 기능 완료  - context api 로 작성한 이유: redux는 순수함수로 작성되어야하는데 modal 로직은 component를 인자로 넘겨주는 방식임으로 redux와 어울리지않음.  - home page에서 test할 수 있게 데모 ui 작성

--- src/app/(rootLayout)/(home)/page.tsx
+++ src/app/(rootLayout)/(home)/page.tsx
@@ -1,5 +1,12 @@
+import SignupTest from '@/components/pages/signupTest';
+
 const Home = () => {
-  return <>Home</>;
+  return (
+    <div className="flex flex-col">
+      <h1>Home</h1>
+      <SignupTest />
+    </div>
+  );
 };
 
 export default Home;
