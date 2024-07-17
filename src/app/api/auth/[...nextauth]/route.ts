ce36f04f0757bb7780e217deb3f4a12d5450ac9a
yeji Sung <dpwl0332@naver.com>
2024-07-17 18:36:42 +0900

    [#31] fix : authorize 에러 해결 및 json 오류 해결 중

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,36 +11,28 @@ const handler = NextAuth({
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
-        try {
-          const res = await fetch(
-            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users/login`,
-            {
-              method: 'POST',
-              body: JSON.stringify(credentials),
-              headers: {
-                'Content-Type': 'application/json',
-                Origin: 'http://localhost:3000',
-              },
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users/login`,
+          {
+            method: 'POST',
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+              Origin: 'http://localhost:3000',
             },
-          );
+          },
+        );
 
-          const data = await res.json();
-          console.log('Received response from backend:', data);
-
-          if (res.ok && data) {
-            // Any object returned will be saved in `user` property of the JWT
-            return data;
-          } else {
-            // If you return null or false then the credentials will be rejected
-            return null;
-            // You can also Reject this callback with an Error or with a URL:
-            // throw new Error('error message') // Redirect to error page
-            // throw '/path/to/redirect'        // Redirect to a URL
-          }
-        } catch (error) {
-          console.log(error);
-          throw new Error('error message');
+        const user = await res.json();
+        console.log(user, 'user');
+        if (res.ok && user) {
+          return user;
         }
+        // Return null if user data could not be retrieved
+        return null;
       },
     }),
   ],
@@ -50,11 +42,11 @@ const handler = NextAuth({
       if (user) {
         token.accessToken = user.token;
       }
-      return token;
+      return { ...token, ...user };
     },
     async session({ session, token }: { session: any; token: JWT }) {
       session.accessToken = token.accessToken;
-      return session;
+      return { ...token, ...session };
     },
   },
   pages: {
