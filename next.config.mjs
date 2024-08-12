17d16a5c83257a26a4420485d3305d4e163722f5
jiisuniui <dudndi9752@gmail.com>
2024-08-13 00:03:10 +0900

    [#67] chore: cicd 구성중

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        // 모든 API 경로에 적용
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' }, // 자격 증명 허용
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://dev.rchive.kr',
-          }, // 특정 도메인만 허용
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, OPTIONS, POST, PUT, PATCH, DELETE',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization, X-Requested-With',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
