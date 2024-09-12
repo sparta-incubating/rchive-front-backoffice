f696ef40788235387634bf52a28682d5ade8c9db
yeji Sung <dpwl0332@naver.com>
2024-09-13 03:09:19 +0900

    [#155]chore : table div로 수정

--- src/components/atoms/admin/adminTableHeader.tsx
+++ src/components/atoms/admin/adminTableHeader.tsx
@@ -10,36 +10,35 @@ const AdminTableHeader = ({
   isAllChecked,
 }: TableHeaderProps) => {
   return (
-    <thead>
-      <tr className="flex h-[64px] w-[1012px] items-center border-b border-blue-100 pl-6 text-sm">
-        <td className="mr-7 flex h-5 w-5 items-center justify-center">
+    <div>
+      <div className="flex h-[64px] w-[1012px] items-center border-b border-blue-100 pl-6 text-sm">
+        <div className="mr-7 flex h-5 w-5 items-center justify-center">
           <CategoryBox
             text=""
             onChange={(e) => handleAllCheck(e.target.checked)}
             checked={isAllChecked}
           />
-        </td>
-
-        <td className="flex h-[56px] w-[118px] items-center px-[10px] text-sm font-medium text-gray-400">
+        </div>
+        <div className="flex h-[56px] w-[118px] items-center px-[10px] text-sm font-medium text-gray-400">
           이름
-        </td>
-        <td className="flex h-[56px] w-[118px] items-center px-[10px] text-sm font-medium text-gray-400">
+        </div>
+        <div className="flex h-[56px] w-[118px] items-center px-[10px] text-sm font-medium text-gray-400">
           직책
-        </td>
-        <td className="flex h-[56px] w-[118px] items-center px-[10px] text-sm font-medium text-gray-400">
+        </div>
+        <div className="flex h-[56px] w-[118px] items-center px-[10px] text-sm font-medium text-gray-400">
           기수
-        </td>
-        <td className="flex h-[56px] w-[268px] items-center px-[16px] text-sm font-medium text-gray-400">
+        </div>
+        <div className="flex h-[56px] w-[268px] items-center px-[16px] text-sm font-medium text-gray-400">
           이메일
-        </td>
-        <td className="flex h-[56px] w-[171px] items-center text-sm font-medium text-gray-400">
+        </div>
+        <div className="flex h-[56px] w-[171px] items-center text-sm font-medium text-gray-400">
           요청날짜
-        </td>
-        <td className="flex h-[56px] w-[123px] items-center px-[10px] text-sm font-medium text-gray-400">
+        </div>
+        <div className="flex h-[56px] w-[123px] items-center px-[10px] text-sm font-medium text-gray-400">
           승인상태
-        </td>
-      </tr>
-    </thead>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/pages/admin/AuthFilteredList.tsx
+++ src/components/pages/admin/AuthFilteredList.tsx
@@ -44,7 +44,7 @@ const AuthFilteredList = ({ data }: FilteredListProps) => {
     <div className="h-[471px] border-gray-300">
       {data.length > 0 ? (
         data.map((item) => (
-          <tr
+          <div
             key={item.email + item.period}
             className={`flex h-[64px] items-center border-b text-sm hover:bg-blue-50 ${
               adminIds.findIndex(
@@ -55,7 +55,7 @@ const AuthFilteredList = ({ data }: FilteredListProps) => {
                 : ''
             }`}
           >
-            <td className="ml-6 mr-7 flex h-5 w-5 items-center justify-center">
+            <div className="ml-6 mr-7 flex h-5 w-5 items-center justify-center">
               <CategoryBox
                 text=""
                 onChange={handleCheckChange({
@@ -70,26 +70,26 @@ const AuthFilteredList = ({ data }: FilteredListProps) => {
                   ) !== -1
                 }
               />
-            </td>
-            <td className="flex h-[64px] w-[118px] items-center pl-[10px] pr-[16px] text-sm font-medium text-gray-700">
+            </div>
+            <div className="flex h-[64px] w-[118px] items-center pl-[10px] pr-[16px] text-sm font-medium text-gray-700">
               {item.username}
-            </td>
-            <td className="flex h-[64px] w-[118px] items-center pl-[10px] pr-[16px] text-sm font-medium text-gray-700">
+            </div>
+            <div className="flex h-[64px] w-[118px] items-center pl-[10px] pr-[16px] text-sm font-medium text-gray-700">
               {item.trackRole === 'STUDENT' ? '수강생' : item.trackRole}
-            </td>
-            <td className="flex h-[64px] w-[118px] items-center pl-[10px] pr-[16px] text-sm font-medium text-gray-700">
+            </div>
+            <div className="flex h-[64px] w-[118px] items-center pl-[10px] pr-[16px] text-sm font-medium text-gray-700">
               {item.period}
-            </td>
-            <td className="flex h-[64px] w-[268px] items-center px-[16px] py-[12px] text-sm font-medium text-gray-700">
+            </div>
+            <div className="flex h-[64px] w-[268px] items-center px-[16px] py-[12px] text-sm font-medium text-gray-700">
               {item.email}
-            </td>
-            <td className="flex h-[64px] w-[171px] items-center text-sm font-medium text-gray-700">
+            </div>
+            <div className="flex h-[64px] w-[171px] items-center text-sm font-medium text-gray-700">
               {item.createdAt}
-            </td>
-            <td className="flex h-[64px] w-[123px] items-center px-[10px] text-sm font-medium text-gray-700">
+            </div>
+            <div className="flex h-[64px] w-[123px] items-center px-[10px] text-sm font-medium text-gray-700">
               <AdminSelectBoxCategory dataList={item} isStatus={item.auth} />
-            </td>
-          </tr>
+            </div>
+          </div>
         ))
       ) : (
         <NoDataList />
